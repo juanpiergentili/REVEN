54ae634da36f486b69f2ab4e746c93269ab02907
Juan Pablo <juanpablo.piergentili@gmail.com>
2026-05-02 19:19:33 -0300

    fix: remove + icon from REGISTRARSE button

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -292,7 +292,6 @@ export function Header() {
                   onClick={() => navigate('/?register=true')}
                 >
                   REGISTRARSE
-                  <Plus className="ml-2 h-4 w-4 group-hover:rotate-90 transition-transform" />
                 </Button>
               </>
             )}
